051c14e3b7c3db00334c7513b97dd987d7240cfd
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-05-09 15:53:44 -0300

    Primeiro commit

---
+++ backend/src/test/java/com/dvsuperior/devsuperior/DevsuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.devsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
